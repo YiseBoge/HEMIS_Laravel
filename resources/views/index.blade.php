4b957d5e471f3180e4bb4d66dd284c790425aadd
yisehakbogale <yisehakb@gmail.com>
2019-04-13 21:58:24 +0300

    Setup Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content">
+        <div class="title m-5 display-1 text-center">
+            HEMIS
+        </div>
+
+    </div>
+@stop
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name', 'HEMIS')}}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
+</head>
+<body>
+    @include('inc.navbar')
+
+    @yield('content', 'Nothing to Show')
+</body>
+</html>
