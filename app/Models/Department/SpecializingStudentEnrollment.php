e5f7d782cfefe35326580096a7e8a82041d15d2e
LydiaGA <lydiagashawtena@gmail.com>
2019-05-11 17:33:08 +0300

    Add Specializing Student

---
+++ app/Models/Department/SpecializingStudentEnrollment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Department;
+
+use App\Traits\Enums;
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecializingStudent extends Model
+{
+    use Uuids;
+    use Enums;
+
+    public $incrementing = false;
+    protected $enumStudentTypes = [
+        'CURRENT' => 'Current',
+        'PREVIOUS_YEAR' => 'Previous Year'
+    ];
+
+    protected $enumSpecializationTypes = [
+        'SPECIALIZATION' => 'Specialization',
+        'SUB_SPECIALIZATION' => 'Sub Specialization'
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo('App\Models\Department\Department');
+    }
+}
---
+++ database/migrations/2019_05_11_141340_create_specializing_students_enrollment_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpecializingStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specializing_students', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->bigInteger('male_students_number');
+            $table->bigInteger('female_students_number');
+            $table->string('field_of_specialization');
+            $table->timestamps();
+
+            $table->string('student_type');
+            $table->string('specialization_type');
+
+            $table->primary('id');
+
+            $table->uuid('department_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specializing_students');
+    }
+}
