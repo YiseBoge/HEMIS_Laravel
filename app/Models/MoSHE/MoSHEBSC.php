b00d77169d31f67ee5d6cfa0ef67fffed0da3100
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-06-15 10:42:13 +0300

    Added the MosHEBSC model and migration

---
+++ app/Models/MoSHE/MoSHEBSC.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\MoSHE;
+
+use App\Traits\Enums;
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Model;
+class MoSHEBSC extends Model
+{
+    use Uuids;
+    use Enums;
+    public $incrementing = false;
+
+    protected $enumCategories = [
+        'CAT-I' => 'Improve access & equity',
+        'CAT-II' => 'Improve internal efficiency',
+        'CAT-III' => 'Improve quality & relevance of education',
+        'CAT-IV' => 'Improve quality & relevance of research',
+        'CAT-V' => 'Promote diversity in higher education institutions',
+        'CAT-VI' => 'Improve internationalization',
+        'CAT-VII' => 'Improve resources mobilization',
+    ];
+
+    
+}
