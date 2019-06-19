0d695e14975387b1aa8227ef860bf21467b2a4e2
aymenjelal <aymen2jelal@gmial.com>
2019-06-19 12:08:27 +0300

    number added some department service functions

--- app/Services/DepartmentService.php
+++ app/Services/DepartmentService.php
@@ -15,4 +15,23 @@ class DepartmentService
 
     // functions go here dependent on the department
     // can make functions to take in strings 'postgraduate', or 'undergraduate' then return accordingly
+
+    function expatriateStaff(){
+        $total = 0;
+        foreach ($this->department->expatriates as $expatriate){
+            $total += $expatriate->male_number + $expatriate->female_number;
+        }
+        return $total;
+
+    }
+
+    function academicStaffPublication(){
+        $total = 0;
+        foreach ($this->department->academicStaffs as $academicStaff){
+            foreach ($academicStaff->publications as $publication){
+                $total= $total +1;
+
+            }
+        }
+    }
 }
