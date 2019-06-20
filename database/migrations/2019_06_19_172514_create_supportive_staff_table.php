6a08c2879ea1238ab5e3052ddaf81df23895157b
aymenjelal <aymen2jelal@gmial.com>
2019-06-20 17:02:12 +0300

    pulling from new dev

---
+++ database/migrations/2019_06_19_172514_create_supportive_staff_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSupportiveStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supportive_staff', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->text('staffRank');
+            $table->timestamps();
+
+            $table->primary('id');
+
+            $table->uuid('college_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supportive_staff');
+    }
+}
