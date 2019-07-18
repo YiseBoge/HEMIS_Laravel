a21406dfd664a92488dbb149c908e2cb89a88b6e
yiseboge <Ethiopia1!>
2019-07-18 15:33:06 +0300

    modify notification messages

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,44 +1,52 @@
 @if(count($errors) > 0)
-    @foreach($errors->all() as $error)
-        <div class="alert alert-danger">
-            {{$error}}
-        </div>
-    @endforeach
+    <div class="container-fluid p-0 px-md-4">
+        @foreach($errors->all() as $error)
+            <div class="alert alert-danger">
+                {{$error}}
+            </div>
+        @endforeach
+    </div>
 @endif
 
 
 @if (session('success'))
-    <div class="card border-0 shadow-none">
-        <div class="fade-alert alert alert-success text-center shadow w-100">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                ×
-            </button>
-            <i class="far fa-check-circle fa-sm d-inline-block mx-2"></i>
-            <strong>Hurray!!</strong> {{ session('success') }}
+    <div class="container-fluid p-0 px-md-4">
+        <div class="card border-0 shadow-none">
+            <div class="fade-alert alert alert-success text-center shadow w-100">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    ×
+                </button>
+                <i class="far fa-check-circle fa-sm d-inline-block mx-2"></i>
+                <strong>Hurray!!</strong> {{ session('success') }}
+            </div>
         </div>
     </div>
 @endif
 
 @if (session('primary'))
-    <div class="card border-0 shadow-none">
-        <div class="fade-alert alert alert-primary text-center shadow w-100">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                ×
-            </button>
-            <i class="far fa-check-circle fa-sm d-inline-block mx-2"></i>
-            {{ session('primary') }}
+    <div class="container-fluid p-0 px-md-4">
+        <div class="card border-0 shadow-none">
+            <div class="fade-alert alert alert-primary text-center shadow w-100">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    ×
+                </button>
+                <i class="far fa-check-circle fa-sm d-inline-block mx-2"></i>
+                {{ session('primary') }}
+            </div>
         </div>
     </div>
 @endif
 
 @if (session('error'))
-    <div class="card border-0 shadow-none">
-        <div class="fade-alert alert alert-danger text-center shadow w-100">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                ×
-            </button>
-            <i class="fas fa-times fa-sm d-inline-block mx-2"></i>
-            <strong>Oops!!</strong> {{ session('error') }}
+    <div class="container-fluid p-0 px-md-4">
+        <div class="card border-0 shadow-none">
+            <div class="fade-alert alert alert-danger text-center shadow w-100">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                    ×
+                </button>
+                <i class="fas fa-times fa-sm d-inline-block mx-2"></i>
+                <strong>Oops!!</strong> {{ session('error') }}
+            </div>
         </div>
     </div>
 @endif
