d37f4788a8e89deae0a699690bc0b4c2a470a793
yiseboge <Ethiopia1!>
2019-07-29 00:47:17 +0300

    build skeleton models, views and controllers for institution reports

---
+++ database/migrations/2019_07_28_195723_create_institution_year_values_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstitutionYearValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('institution_year_values', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('year');
+            $table->double('value')->default(0);
+            $table->timestamps();
+
+            $table->primary('id');
+
+            $table->uuid('institution_report_card_id');
+            $table->uuid('institution_name_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('institution_year_values');
+    }
+}
