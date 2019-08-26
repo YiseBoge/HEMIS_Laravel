e190be42d31ee6f00c44f95c81a5d36f83957182
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-08-26 19:06:13 +0300

    Minor modifications and corrected some errors

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuids;
 
 class Comment extends Model
 {
-    //
+    use Uuids;
+    public $incrementing = false; 
+
+    protected $casts = [
+        'created_at' => 'date:Y-m-d H:00'
+    ];
 
     public function user(){
         return $this->belongsTo('App\User');
